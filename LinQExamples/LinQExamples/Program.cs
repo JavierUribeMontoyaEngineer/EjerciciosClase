eabde06aa5c7fe23ce4816357ea3c82121ef2d2a
JavierUribeMontoyaEngineer <javier.uribe@coitimur.org>
2017-08-03 15:23:41 +0200

    Between

--- LinQExamples/LinQExamples/Program.cs
+++ LinQExamples/LinQExamples/Program.cs
@@ -12,6 +12,8 @@ namespace LinQExamples
                 1,2,3
             };
             var a = 10;
+            Console.WriteLine("Caso extender int con funcion between");
+            Console.WriteLine(a.BetWeen(9, 10));
             Console.WriteLine("Caso con funcion Predicate");
             var bigNumbersPredicate = numbers.Where(n => n > 1);
             foreach (var number in bigNumbersPredicate)
