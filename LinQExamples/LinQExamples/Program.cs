7c03d48f33bb513357d8df601c2a865c7622f0d6
Javier Uribe <Usuario@DESKTOP-612>
2017-08-03 11:44:06 +0200

    Proyectoy para ejemplos LinQ

---
+++ LinQExamples/LinQExamples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LinQExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[1, 2, 3];
+
+        }
+    }
+}
