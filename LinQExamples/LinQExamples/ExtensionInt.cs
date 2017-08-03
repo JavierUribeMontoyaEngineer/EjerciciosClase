c0d7daa386a79968951d7006bedd1dead6736496
Javier Uribe <Usuario@DESKTOP-612>
2017-08-03 12:24:20 +0200

    2 formas de hacer consultas. Implementando el where o usando lambda

---
+++ LinQExamples/LinQExamples/ExtensionInt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinQExamples
+{
+    public static class ExtensionInt
+    {
+        public static bool BetWeen(this int value, int from, int to)
+        {
+            return value >= from && value <= to;
+        }
+    }
+}
--- LinQExamples/LinQExamples/Program.cs
+++ LinQExamples/LinQExamples/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace LinQExamples
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new int[1, 2, 3];
+            var numbers = new List<int>()
+            {
+                1,2,3
+            };
+            var bigNumbers = numbers.Where(Predicate);
+            bigNumbers = numbers.Where(n => n > 1);
+            var a = 10;
+            a.BetWeen(2, 20);
+            
 
+        }
+
+        static bool Predicate(int value)
+        {
+            return value > 1;
         }
     }
 }
